9207bf351edb53e21f21054666243d125e47d63c
JuleeSYX <sayaxang1998@gmail.com>
2023-09-11 08:30:48 +0700

    up

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,13 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router} from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+export const AuthGuard: CanActivateFn = (route, state) =>{
+  const cookieService = inject(CookieService);
+  const token = cookieService.get("auth-token")
+  const router = inject(Router);
+  if(token != '' && token != null){
+    return true;
+  }
+  router.navigateByUrl('/login')
+  return false;
+}
